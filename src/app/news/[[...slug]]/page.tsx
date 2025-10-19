6c6ac3c4330408706a56d722bf99f908e80cf06d
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-19 18:38:12 +0600

    coustom not found page learn

--- src/app/news/[[...slug]]/page.tsx
+++ src/app/news/[[...slug]]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 const NewsDetails = async({params} : {params: Promise<{slug:string[]}>}) => {
   const {slug} = await params
   // console.log(slug)
+
+  if(slug.length > 3){
+    notFound()
+  }
 
   if(slug?.length === 2) {
     return <p>Viewing product for category: {slug[0]} and product: {slug[1]}</p>
