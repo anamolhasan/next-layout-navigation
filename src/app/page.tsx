b24ea69f5f18314ca9cc65581ff5137dcd93471a
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-19 21:04:10 +0600

    frist app ended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-   <div>
-     <p>This is Home Page</p>
+   <div className="flex items-center justify-center min-h-screen">
+     <p className="text-5xl font-bold">Home Page</p>
    </div>
   );
 }
