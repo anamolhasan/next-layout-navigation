7ef9d227d233c6f18fb0f22476dbddd67ab42e80
anamolhasan <mdanamolhasan1998@gmail.com>
2025-12-30 21:01:25 +0600

    update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
+
+
+const navItems = [
+  {name:'Home', href:'/'},
+  {name:'About', href:'/about'},
+  {name:'Contact', href:'/contact'},
+  {name:'Blog', href:'/blog'},
+]
 
 const Navbar = () => {
   return (
-    <div className="text-2xl font-bold text-center py-4 bg-green-800">Navbar</div>
+    <div className=" font-bold text-center py-4 bg-green-800">
+      <div >
+         <Link href='/'>Bike Sharing</Link>
+      </div>
+      <ul>
+        {navItems.map((item, index) => (
+          <li key={index}> 
+             <Link href={item.href}>{item.name}</Link>
+           </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
